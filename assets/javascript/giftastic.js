042fdb76047c23a6c59ba36f08cb61517f634e89
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-12 16:47:54 -0700

    Created a pseudocode instructions for giftastic.js prior to executing the codes

---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,11 @@
+//Make a variable called gifs with an array of well-known celebrities.
+//Create a .ready function so the javascript can fully load when the browser loads.
+//Create an click function for the button to execute that function when clicked.
+//Make a variable called gif with a value of "this" attribute.
+//Make a variable called queryURL for the URL link to the giphy api site.
+//Create a function that will send request to the server then console log it.
+//Make a for loop that will add the gif images to the html. Also include data-still and data-animate.
+//Append the gif images to the html.
+//Create a renderButtons function so buttons will appear when the name is searched.
+//Create an click function that will display the gifs once the button is clicked.
+
