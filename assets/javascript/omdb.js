84bf697e650b270c9506e48af09f460f194a1c1e
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-13 08:02:21 -0700

    via omdb.js, I created a  list of variable inputs for movies from the object response and appended them to the html using jQuery

--- assets/javascript/omdb.js
+++ assets/javascript/omdb.js
@@ -1,8 +1,42 @@
 var movies = ["Batman", "Little Manhattan", "August Rush", "Toy Story"];
 
-function showMovie(){
+function showMovie() {
     var movie = $(this).attr("data-name");
     var queryURL = "https://www.omdbapi.com/?=" + movie + "&y=&plot=short&apikey=4445d410";
 
-    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var movieDiv = $("<div class = 'movie'>");
+            console.log(response);
+
+            var title = response.Title;
+            var rating = response.Rated;
+            var released = response.Released;
+            var plot = response.Plot;
+            var image = response.Poster;
+
+            $("#movies-view").empty();
+
+            var p = $("<h2>").text("Title: " + title);
+            movieDiv.append(p);
+
+            var rated = $("<p>").text("Rated: " + rating);
+            movieDiv.append(rated);
+
+            var date = $("<p>").text("Released: " + released);
+            movieDiv.append(date);
+
+            var summary = $("<p>").text("Released: " + plot);
+            movieDiv.append(summary);
+
+            var picture = $("<img>").attr("src", image);
+            movieDiv.append(picture);
+
+            $("#movies-view").append(movieDiv);
+
+        })
+
 }
