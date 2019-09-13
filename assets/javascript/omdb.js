b48582585518ce7f0f80591257bcf654cccfc85a
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-13 09:04:33 -0700

    Added html format for bandsintown.html

--- assets/javascript/omdb.js
+++ assets/javascript/omdb.js
@@ -1,72 +1,73 @@
-var movies = [];
-$(document).ready(function (){
+var movies = ["Toy Story", "Like Mike", "Space Jam"];
+$(document).ready(function () {
 
 
-function showMovie() {
-    var movie = $(this).attr("data-name");
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=4445d410";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            var movieDiv = $("<div class = 'movie'>");
-            console.log(response);
+    function showMovie() {
+        var movie = $(this).attr("data-name");
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=4445d410";
 
-            var title = response.Title;
-            var rating = response.Rated;
-            var released = response.Released;
-            var plot = response.Plot;
-            var image = response.Poster;
-
-            $("#movies-view").empty();
-
-            var p = $("<h2>").text(title);
-            movieDiv.append(p);
-
-            var rated = $("<p>").text("Rated: " + rating);
-            movieDiv.append(rated);
-
-            var date = $("<p>").text("Released: " + released);
-            movieDiv.append(date);
-
-            var summary = $("<p>").text("Released: " + plot);
-            movieDiv.append(summary);
-
-            var picture = $("<img>").attr("src", image);
-            movieDiv.append(picture);
-
-            $("#movies-view").prepend(movieDiv);
-
+        $.ajax({
+            url: queryURL,
+            method: "GET"
         })
+            .then(function (response) {
+                var movieDiv = $("<div class = 'movie'>");
+                console.log(response);
 
-}
+                var title = response.Title;
+                var rating = response.Rated;
+                var released = response.Released;
+                var plot = response.Plot;
+                var image = response.Poster;
 
-function renderButtons() {
-    $("#movie-view").empty();
+                $("#movies-view").empty();
 
-    for (var i = 0; i < movies.length; i++) {
-        var newButton = $("<button>");
-        newButton.addClass("movieButton");
-        newButton.attr("data-name", movies[i]);
-        newButton.text(movies[i]);
-        $("#movie-view").append(newButton);
+                var p = $("<h2>").text(title);
+                movieDiv.append(p);
+
+                var rated = $("<p>").text("Rated: " + rating);
+                movieDiv.append(rated);
+
+                var date = $("<p>").text("Released: " + released);
+                movieDiv.append(date);
+
+                var summary = $("<p>").text("Released: " + plot);
+                movieDiv.append(summary);
+
+                var picture = $("<img>").attr("src", image);
+                movieDiv.append(picture);
+
+                $("#movies-view").prepend(movieDiv);
+
+            })
+
     }
-}
+
+
+    function renderButtons() {
+        $("#movie-view").empty();
+
+        for (var i = 0; i < movies.length; i++) {
+            var newButton = $("<button>");
+            newButton.addClass("movieButton");
+            newButton.attr("data-name", movies[i]);
+            newButton.text(movies[i]);
+            $("#movie-view").append(newButton);
+        }
+    }
 
 
 
-$("#add-movie").on("click", function (event) {
-    event.preventDefault();
+    $("#add-movie").on("click", function (event) {
+        event.preventDefault();
 
-    var movie = $("#movie-input").val().trim();
-    movies.push(movie);
+        var movie = $("#movie-input").val().trim();
+        movies.push(movie);
 
-    renderButtons();
-});
-$(document).on("click", ".movieButton", showMovie);
+        renderButtons();
+    });
+    $(document).on("click", ".movieButton", showMovie);
 
-renderbuttons();
-
+    renderbuttons();
 });
