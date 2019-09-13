d003a2a14d5744c93657b5c111ecd2cc093d7e4c
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-13 07:49:04 -0700

    Added a variable with a value of an array of movies and a queryURL with omdb api key

--- assets/javascript/omdb.js
+++ assets/javascript/omdb.js
@@ -1,2 +1,8 @@
 var movies = ["Batman", "Little Manhattan", "August Rush", "Toy Story"];
 
+function showMovie(){
+    var movie = $(this).attr("data-name");
+    var queryURL = "https://www.omdbapi.com/?=" + movie + "&y=&plot=short&apikey=4445d410";
+
+    
+}
